a66a3c9c6aeafb1b6ee589d556d7198a756918fd
Fabio Moura <fclmrj@msn.com>
2019-12-05 10:01:33 -0200

    Moving controllers classes to package org.springmvc.controllers

--- src/main/java/org/springmvc/config/AppWebConfiguration.java
+++ src/main/java/org/springmvc/config/AppWebConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springmvc.HomeController;
+import org.springmvc.controllers.HomeController;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses={HomeController.class})
